43582fcd95ccdc7e815ab88b227820135ca70eaa
ayns1688 <mostafanaghiha3@gmail.com>
2025-11-22 01:50:41 -0800

    Add files via upload

---
+++ motosadish.php
@@ -0,0 +1,18 @@
+    <!-- header -->
+<?php
+include("header.html")
+?>
+    <header class="hn">
+        <div>
+            <p>
+                شهر های مبارکه و فولاد شهر و نجف آباد
+            </p>
+        </div>
+        <h4>امیر نقی ها</h4>
+        <h4>محمد نقی ها</h4>
+        <h4>ابراهیم نقی ها</h4>
+    </header>
+    <!-- footer -->
+<?php
+include("footer.html")
+?>
